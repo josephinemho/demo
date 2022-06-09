53c0b78bf4da55aa965747c65b458e89c777df84
josephinemho@gmail.com <josephinemho@gmail.com>
2022-06-09 16:23:04 +0000

    added new demo notebook

---
+++ demo (1).py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+df = table("diamonds_9_csv")
+display(df)
+
+# COMMAND ----------
+
+df.show()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from diamonds_9_csv
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from diamonds_9_csv
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from diamonds_9_csv
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from airbnb_demo_db.airbnb_cleaned_csv
+
+# COMMAND ----------
+
+
